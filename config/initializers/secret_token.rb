b5c1c877f204ed9f9088479a1e7b294979a1f679
gruntrebel <aaronslezak@gmail.com>
2014-03-12 16:12:49 -0400

    initial commit, did some beginning work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WcccGaming::Application.config.secret_key_base = '2bc76540e3fccfff6953a5e43f60241b3d8cad4900a0c3c855003b155afb35dc47a26326dc2e43f8ca84796c48600e19732f6c8b51cfe2693871da1c5efd6df9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WcccGaming::Application.config.session_store :cookie_store, key: '_wccc-gaming_session'
