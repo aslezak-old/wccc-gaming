2cb2fb24f190c4e66d88b2b58523d3621de40499
gruntrebel <gruntrebel@dev.(none)>
2014-03-12 21:10:47 -0400

    get base logic down

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,45 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def new
+  end
+
+  def index
+    @posts = Post.all
   end
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+
+  redirect_to posts_path
+end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
 private
