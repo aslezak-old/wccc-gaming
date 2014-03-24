6338abffa430f39e0e87e1fdc1948bdb7d7a95f0
gruntrebel <gruntrebel@dev2-VirtualBox.(none)>
2014-03-24 14:27:03 -0400

    work on basic sign up form functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 WcccGaming::Application.routes.draw do
   devise_for :users
   resources :posts
+  resources :inquiries, :only => [:new, :create] do
+    get 'thank_you', :on => :collection
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
