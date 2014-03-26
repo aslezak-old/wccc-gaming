9c5f3fd0ea72859b7659b3d950802557702908d4
gruntrebel <gruntrebel@dev2-VirtualBox.(none)>
2014-03-26 10:33:47 -0400

    clean up some spacing issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,37 +1,37 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :destroy, :create, :update, :new]
 
-  def new
-    @post = Post.new
-  end
+def new
+  @post = Post.new
+end
 
-  def index
-    @pagi = Post.paginate(:page => params[:page], :per_page => 3, :order => 'created_at desc')
-    @events = Post.where('is_event = "t"').reorder('event_date ASC').take(5)
-  end
+def index
+  @pagi = Post.paginate(:page => params[:page], :per_page => 3, :order => 'created_at desc')
+  @events = Post.where('is_event = "t"').reorder('event_date ASC').take(5)
+end
 
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to @post
-    else
-      render 'new'
-    end
+def create
+  @post = Post.new(post_params)
+  if @post.save
+    redirect_to @post
+  else
+    render 'new'
   end
+end
 
-  def edit
-    @post = Post.find(params[:id])
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
   end
-
-  def update
-    @post = Post.find(params[:id])
-
-    if @post.update(post_params)
-      redirect_to @post
-    else
-      render 'edit'
-    end
-  end
+end
 
 def destroy
   @post = Post.find(params[:id])
