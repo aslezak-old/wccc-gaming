d6bf4030d0cc645de3e60309e44ff0cd5f5039d0
gruntrebel <gruntrebel@dev2-VirtualBox.(none)>
2014-03-27 12:42:36 -0400

    add secret for heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-WcccGaming::Application.config.secret_key_base = '2bc76540e3fccfff6953a5e43f60241b3d8cad4900a0c3c855003b155afb35dc47a26326dc2e43f8ca84796c48600e19732f6c8b51cfe2693871da1c5efd6df9'
+secret = Rails.env.production? ? ENV['SECRET_TOKEN'] : "top_secret_token"
+WcccGaming::Application.config.secret_key_base = secret
